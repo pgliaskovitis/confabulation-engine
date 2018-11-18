7e2c79953284c45ed46276e2483d27cebcea9da3
Periklis Liaskovitis <pgliaskovitis@gmail.com>
2018-11-18 14:12:33 +0200

    Take into account reference frames for phrase initialization as well

--- TwoLevelMultiConfabulation.cpp
+++ TwoLevelMultiConfabulation.cpp
@@ -19,7 +19,7 @@ TwoLevelMultiConfabulation::TwoLevelMultiConfabulation(size_t num_word_modules,
 		kb_specs[i].resize(num_modules_);
 	}
 
-	// word-to-future-word knowledge bases (reference frame length ahead)
+	// word-to-future-word knowledge bases (reference frame length ahead - affects initialization)
 	for (size_t i = 0; i < num_word_modules; ++i) {
 		for (size_t j = i + 1; j < num_word_modules && j < i + 1 + Globals::kReferenceFrameSize; ++j) {
 			kb_specs[i][j] = true;
@@ -33,9 +33,9 @@ TwoLevelMultiConfabulation::TwoLevelMultiConfabulation(size_t num_word_modules,
 		}
 	}
 
-	// word-to-future-phrase knowledge bases (max phrase length ahead)
+	// word-to-future-phrase knowledge bases (max phrase length ahead - affects initialization)
 	for (size_t i = 0; i < num_word_modules; ++i) {
-		for (size_t j = num_word_modules + i + 1; j < 2 * num_word_modules && j < num_word_modules + i + 1 + Globals::kMaxMultiWordSize; ++j) {
+		for (size_t j = num_word_modules + i + 1; j < 2 * num_word_modules && j < num_word_modules + i + 1 + Globals::kReferenceFrameSize; ++j) {
 			kb_specs[i][j] = true;
 		}
 	}
